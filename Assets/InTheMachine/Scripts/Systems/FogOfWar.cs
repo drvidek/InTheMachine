0f5315e44e13dfeac0a03069f76ad176e9227200
drvidek <simeon.yialeloglou@me.com>
2024-04-11 19:38:04 +1000

    FogOfWar map remake
    No worky

--- Assets/InTheMachine/Scripts/Systems/FogOfWar.cs
+++ Assets/InTheMachine/Scripts/Systems/FogOfWar.cs
@@ -7,7 +7,7 @@ public class FogOfWar : MonoBehaviour
 {
     [SerializeField] private bool chargePoint;
 
-    private MachineTilemap fogMap;
+    private Tilemap fogMap;
 
     private static int tilesToClear;
 
@@ -20,43 +20,43 @@ public class FogOfWar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        fogMap = GetComponent<MachineTilemap>();
+        fogMap = GetComponent<Tilemap>();
         RoomManager.main.onPlayerMovedRoom += ClearFog;
         Checkpoint.onActivate += ClearArea;
     }
 
     private void ClearFog(Vector3Int room)
     {
-        //Debug.Log($"Trying to clear {room}");
-        //if (!clearingArea && (tilesToClear <= 0 && chargePoint))
-        //{
-        //    Debug.LogAssertion($"Failed to clear: Area?{clearingArea} Pips?{tilesToClear} Cost?{chargePoint}");
-        //    return;
-        //}
-        //
-        //Vector3 roomPos = RoomManager.main.RoomGrid.CellToWorld(room);
-        //Vector3Int fogTile = fogMap.WorldToCell(roomPos);
-        //fogTile.z = 0;
-        //
-        //if (fogMap.GetTile(fogTile) == null)
-        //{
-        //    Debug.LogAssertion($"Did not find tile on {fogMap} at {roomPos}");
-        //
-        //    return;
-        //}
-        //
-        //fogMap.SetTile(fogTile, null);
-        //
-        //if (!clearingArea)
-        //{
-        //    fogMap.RefreshAllTiles();
-        //    if (chargePoint)
-        //    {
-        //        onMapReveal?.Invoke();
-        //        tilesToClear--;
-        //    }
-        //}
-        //Debug.LogAssertion($"Cleared tile");
+        Debug.Log($"Trying to clear {room}");
+        if (!clearingArea && (tilesToClear <= 0 && chargePoint))
+        {
+            Debug.LogAssertion($"Failed to clear: Area?{clearingArea} Pips?{tilesToClear} Cost?{chargePoint}");
+            return;
+        }
+        
+        Vector3 roomPos = RoomManager.main.RoomGrid.CellToWorld(room);
+        Vector3Int fogTile = fogMap.WorldToCell(roomPos);
+        fogTile.z = 0;
+        
+        if (fogMap.GetTile(fogTile) == null)
+        {
+            Debug.LogAssertion($"Did not find tile on {fogMap} at {roomPos}");
+        
+            return;
+        }
+        
+        fogMap.SetTile(fogTile, null);
+        
+        if (!clearingArea)
+        {
+            fogMap.RefreshAllTiles();
+            if (chargePoint)
+            {
+                onMapReveal?.Invoke();
+                tilesToClear--;
+            }
+        }
+        Debug.LogAssertion($"Cleared tile");
     }
 
     private void ClearArea(Vector3Int room)
